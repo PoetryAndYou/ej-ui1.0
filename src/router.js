35308d5b4d5bc6f429371e6f3907cd468e4d5a29
ychengrui@163.com <ychengrui@163.com>
2019-06-12 07:55:25 +0800

    初始化

--- src/router.js
+++ src/router.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import UserPage from './routes/UserPage';
-import CoursePage from './routes/CoursePage';
+import OrderLinePage from './routes/OrderLinePage'
+import CustomerPage from './routes/CustomerPage'
+import OrderPage from './routes/OrderPage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
-        <Route path="/user" exact component={UserPage} />
-        <Route path="/course" exact component={CoursePage} />
+        <Route path="/orderLine" exact component={OrderLinePage} />
+        <Route path="/customer" exact component={CustomerPage} />
+        <Route path="/order" exact component={OrderPage} />
       </Switch>
     </Router>
   );
