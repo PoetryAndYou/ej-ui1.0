d9e2caf041305e9e9041a217e49c0652651b099e
wanyuzhou <1227486366@qq.com>
2019-06-13 10:11:18 +0800

    万宇宙第四次修改了router

--- src/router.js
+++ src/router.js
@@ -6,17 +6,22 @@ import CustomerPage from './routes/CustomerPage'
 import OrderPage from './routes/OrderPage'
 import CategoryPage from './routes/CategoryPage'
 import CommentPage from './routes/CommentPage'
+import ProducePage from './routes/ProducePage'
+import WaiterPage from './routes/WaiterPage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Switch>
+      <Switch> 
         <Route path="/" exact component={IndexPage} />
         <Route path="/orderLine" exact component={OrderLinePage} />
         <Route path="/customer" exact component={CustomerPage} />
         <Route path="/order" exact component={OrderPage} />
         <Route path="/category" exact component={CategoryPage} />
         <Route path="/comment" exact component={CommentPage} />
+        <Route path="/orderLine" exact component={OrderLinePage} />
+        <Route path="/produce" exact component={ProducePage} />
+        <Route path="/waiter" exact component={WaiterPage} />
       </Switch>
     </Router>
   );
