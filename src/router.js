2e8b2b2c589ed6df529357596e05186b1dbdf613
ychengrui@163.com <ychengrui@163.com>
2019-06-14 14:54:30 +0800

    主页面

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch ,Link} from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import OrderLinePage from './routes/OrderLinePage'
 import CustomerPage from './routes/CustomerPage'
@@ -9,10 +9,25 @@ import CommentPage from './routes/CommentPage'
 import WaiterPage from './routes/WaiterPage'
 import AddressPage from './routes/AddressPage'
 import ProductPage from './routes/ProductPage'
-
+import styles from './router.css'
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
+      <div className={styles.container}>
+          <div className={styles["left-nav"]}>
+            <div className={styles.title}>E洁家政管理系统</div>
+            <ul>
+              <li className={styles["nav-list-item"]}><Link to="/customer">顾客管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/order">订单管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/category">服务分类管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/comment">评论管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/waiter">服务员管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/address">地址管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/product">服务管理</Link></li>
+            </ul>
+          </div>
+
+          <div className={styles["right-content"]}>
       <Switch> 
         <Route path="/" exact component={IndexPage} />
         <Route path="/orderLine" exact component={OrderLinePage} />
@@ -24,6 +39,8 @@ function RouterConfig({ history }) {
         <Route path="/address" exact component={AddressPage} />
         <Route path="/product" exact component={ProductPage} />
       </Switch>
+      </div>
+      </div>
     </Router>
   );
 }
